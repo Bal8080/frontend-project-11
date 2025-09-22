0c6f3dfd2a8a881630beb4a6eea18be53be0166f
Bal8080 <bb8080t1000@gmail.com>
2025-09-22 18:20:50 +0300

    part 7 fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,10 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
+    languageOptions: { globals: {
+      ...globals.node,
+      ...globals.browser
+      }
+    },
   },
 ])
